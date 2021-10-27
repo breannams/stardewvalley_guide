96218c8ed53b8906ce58ce2b41020a869f7acce5
breannams <breannamarie_s@hotmail.com>
2021-10-27 17:39:16 -0400

    Set up redux store and root reducer.

---
+++ stardew_guide_client/src/Shop/reducers/rootReducer.js
@@ -0,0 +1,28 @@
+import {combineReducers} from 'redux'
+import animalsReducer from './animalsReducer'
+import buildingsReducer from './buildingsReducer'
+import bundlesReducer from './bundlesReducer'
+import cropsReducer from './cropsReducer'
+import festivalsReducer from './festivalsReducer'
+import fishReducer from './fishReducer'
+import mineralsReducer from './mineralsReducer'
+import questsReducer from './questsReducer'
+import recipesReducer from './recipesReducer'
+import shopsReducer from './shopsReducer'
+import treesReducer from './treesReducer'
+import villagersReducer from './villagersReducer'
+
+export const rootReducer = combineReducers({
+    animals: animalsReducer,
+    buildings: buildingsReducer,
+    bundles: bundlesReducer,
+    crops: cropsReducer,
+    festivals: festivalsReducer,
+    fish: fishReducer,
+    minerals: mineralsReducer,
+    quests: questsReducer,
+    recipes: recipesReducer,
+    shops: shopsReducer,
+    trees: treesReducer,
+    villagers: villagersReducer
+})
--- stardew_guide_client/src/index.js
+++ stardew_guide_client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers/rootReducer'
 import App from './App';
 import {Router } from 'react-router-dom';
 import history from './helpers/History';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <Router history = {history} >
-    <App />
-  </Router>,
+  <Provider store = {store}>
+    <Router history = {history} >
+      <App />
+    </Router>
+  </Provider>
+  ,
   document.getElementById('root')
 );
