1ca4779399ae158ae3060a678315f9e5e6093ed1
breannams <breannamarie_s@hotmail.com>
2021-10-27 17:23:10 -0400

    Add building and festival containers.

---
+++ stardew_guide_client/src/containers/BuildingsContainer.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+export class BuildingsContainer extends Component{
+
+    render(){
+        return(
+            <div>Buildings Container</div>
+        )
+    }
+
+}
+
+export default BuildingsContainer
---
+++ stardew_guide_client/src/containers/FestivalsContainer.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+export class FestivalsContainer extends Component{
+
+    render(){
+        return(
+            <div>Festivals Container</div>
+        )
+    }
+
+}
+
+export default FestivalsContainer
