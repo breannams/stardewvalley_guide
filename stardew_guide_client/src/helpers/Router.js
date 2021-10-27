3241e3e1787c95379de96d9bc21cdd4deca39911
breannams <breannamarie_s@hotmail.com>
2021-10-27 18:09:55 -0400

    Update router file to include all containers.

--- stardew_guide_client/src/helpers/Router.js
+++ stardew_guide_client/src/helpers/Router.js
@@ -2,13 +2,18 @@ import { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 
 import HomePage from "../components/pages/HomePage";
-
-// import VillagersContainer from '../containers/VillagersContainer'
-// import BuildingsContainer from '../containers/BuildingsContainer'
-// import AnimalsContainer from '../containers/AnimalsContainer'
-// import FestivalsContainer from '../containers/FestivalsContainer'
-// import QuestsContainer from '../containers/QuestsContainer'
-
+import AnimalsContainer from '../containers/AnimalsContainer'
+import BuildingsContainer from '../containers/BuildingsContainer'
+import BundlesContainer from '../containers/BundlesContainer'
+import CropsContainer from '../containers/CropsContainer'
+import FestivalsContainer from '../containers/FestivalsContainer'
+import FishContainer from '../containers/FishContainer'
+import MineralsContainer from '../containers/MineralsContainer'
+import QuestsContainer  from '../containers/QuestsContainer';
+import RecipesContainer from '../containers/RecipesContainer'
+import ShopsContainer from '../containers/ShopsContainer'
+import TreesContainer from '../containers/TreesContainer'
+import VillagersContainer from '../containers/VillagersContainer'
 
 export class Router extends Component {
     render(){
@@ -17,13 +22,18 @@ export class Router extends Component {
     return(
         <Switch>
             <Route exact path = "/" component = {HomePage} />
-{/* 
-            <Route path = '/villagers' component = {VillagersContainer} />
-            <Route path = '/buildings' component = {BuildingsContainer} />
             <Route path = '/animals' component = {AnimalsContainer} />
+            <Route path = '/buildings' component = {BuildingsContainer} />
+            <Route path = '/bundles' component = {BundlesContainer} />
+            <Route path = '/crops' component = {CropsContainer} />
             <Route path = "/festivals" component = {FestivalsContainer} />
-            <Route path = "/quests" component = {QuestsContainer} /> */}
-          
+            <Route path = '/fish' component = {FishContainer} />
+            <Route path = '/minerals' component = {MineralsContainer} />
+            <Route path = "/quests" component = {QuestsContainer} />
+            <Route path = '/recipes' component = {RecipesContainer} />
+            <Route path = '/shops' component = {ShopsContainer} />
+            <Route path = '/trees' component = {TreesContainer} />
+            <Route path = '/villagers' component = {VillagersContainer} />
         </Switch>
     )
 }}
