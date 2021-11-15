dc2eac9fddd54a6b00407ef27dcf950a386b7f9f
breannams <breannamarie_s@hotmail.com>
2021-11-15 18:44:27 -0500

    Create local state & form for fish form.

--- stardew_guide_client/src/components/Forms/FishForm.js
+++ stardew_guide_client/src/components/Forms/FishForm.js
@@ -1,9 +1,18 @@
 import { Component } from "react";
 import {connect} from 'react-redux'
 
-class Form extends Component {
+class FishForm extends Component {
     state = {
-
+        image: null,
+        name: "",
+        location: "",
+        time: "",
+        season: "",
+        weather: "",
+        difficulty: "",
+        used_in: "",
+        sell_price: 0,
+        stardewvalley_id: null
     }
     handleChange = (event) => {
         this.setState({
@@ -18,9 +27,48 @@ class Form extends Component {
     render(){
         return(
             <>
+                <h1>Add a new fish: </h1>
+                <h3>Choose which Stardewvalley this fish belongs to: </h3> 
+            
+                <StardewmodForm />
+
             <form onSubmit = {this.handleSubmit}>
-                <label>Image:</label>
+                <label>Fish Image:</label>
                 <input type = "file" />
+                <br></br>
+
+                <label>Fish Name: </label>
+                <input type = "text" name = "name" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Location: </label>
+                <input type = "text" name = "location" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Time available: </label>
+                <input type = "text" name = "time" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Season(s) available: </label>
+                <input type = "text" name = "season" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Weather available: </label>
+                <input type = "text" name = "weather" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Difficulty Rating: </label>
+                <input type = "text" name = "difficulty" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Used in: </label>
+                <input type = "text" name = "used_in" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Sell Price: </label>
+                <input type = "number" name = "sell_price" onChange = {this.handleChange} />
+                <br></br>
+
                 <input type = "submit" value = "Create" />
             </form>
             </>
@@ -28,4 +76,4 @@ class Form extends Component {
     }
 }
 
-export default connect(null, {}) (Form)
+export default connect(null, {}) (FishForm)
