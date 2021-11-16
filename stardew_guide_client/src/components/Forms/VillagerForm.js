32f0a698ae8e05932f18262433fcb7f15ce04008
breannams <breannamarie_s@hotmail.com>
2021-11-15 19:44:28 -0500

    Create form & state for villagers.

--- stardew_guide_client/src/components/Forms/VillagerForm.js
+++ stardew_guide_client/src/components/Forms/VillagerForm.js
@@ -1,9 +1,15 @@
 import { Component } from "react";
 import {connect} from 'react-redux'
 
-class Form extends Component {
+class VillagerForm extends Component {
     state = {
-
+        image: null,
+        name: "",
+        gifts: "",
+        heart_event_triggers: [],
+        marriage: false,
+        birthday: "",
+        stardewvalley_id: null
     }
     handleChange = (event) => {
         this.setState({
@@ -18,9 +24,36 @@ class Form extends Component {
     render(){
         return(
             <>
+                <h1>Add a new villager: </h1>
+                <h3>Choose which Stardewvalley this villager belongs to: </h3> 
+                
+                <StardewmodForm />
             <form onSubmit = {this.handleSubmit}>
                 <label>Image:</label>
                 <input type = "file" />
+                <br></br>
+
+                <label>Villager Name: </label>
+                <input type = "text" name = "name" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Birthday: </label>
+                <input type = "text" name = "birthday" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Best Gifts: </label>
+                <input type = "text" name = "gifts" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Heart levels needed for events: </label>
+                <input type = "number" name = "heart_event_triggers" onChange = {this.handleChange} />
+                {/* Need a way to add more to this ^^^^ for an array. */}
+                <br></br>
+
+                <label>Marriagable? </label>
+                Yes<input type = "checkbox" name = "marriage" onChange = {this.handleChange} />
+                <br></br>
+                
                 <input type = "submit" value = "Create" />
             </form>
             </>
@@ -28,4 +61,4 @@ class Form extends Component {
     }
 }
 
-export default connect(null, {}) (Form)
+export default connect(null, {}) (VillagerForm)
