edd05096039f213293c98abfda20dac1be68780c
breannams <breannamarie_s@hotmail.com>
2021-11-15 19:02:46 -0500

    Create local states and forms for heart events and minerals.

--- stardew_guide_client/src/components/Forms/HeartEventForm.js
+++ stardew_guide_client/src/components/Forms/HeartEventForm.js
@@ -1,9 +1,11 @@
 import { Component } from "react";
 import {connect} from 'react-redux'
 
-class Form extends Component {
+class HeartEventForm extends Component {
     state = {
-
+        villager_id: null,
+        heart_number: 0,
+        description: "",
     }
     handleChange = (event) => {
         this.setState({
@@ -18,7 +20,20 @@ class Form extends Component {
     render(){
         return(
             <>
+              <h1>Add a new heart event: </h1>
+                <h3>Choose which Villager this heart belongs to: </h3> 
+                
+               {/* Hmm not sure how I want to handle this one yet. */}
+
             <form onSubmit = {this.handleSubmit}>
+                <label>How many heart levels needed to trigger event: </label>
+                <input type = "number" name = "heart_number" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Event description: </label>
+                <input type = "text" name = "description" onChange = {this.handleChange} />
+                <br></br>
+
                 <input type = "submit" value = "Create" />
             </form>
             </>
@@ -26,4 +41,4 @@ class Form extends Component {
     }
 }
 
-export default connect(null, {}) (Form)
+export default connect(null, {}) (HeartEventForm)
--- stardew_guide_client/src/components/Forms/MineralForm.js
+++ stardew_guide_client/src/components/Forms/MineralForm.js
@@ -1,9 +1,15 @@
 import { Component } from "react";
 import {connect} from 'react-redux'
 
-class Form extends Component {
+class MineralForm extends Component {
     state = {
-
+        image: null,
+        name: "",
+        description: "",
+        sellprice: 0,
+        location: "",
+        used_in: "",
+        stardewvalley_id: null
     }
     handleChange = (event) => {
         this.setState({
@@ -18,9 +24,36 @@ class Form extends Component {
     render(){
         return(
             <>
+            <h1>Add a new mineral: </h1>
+                <h3>Choose which Stardewvalley this mineral belongs to: </h3> 
+                
+                <StardewmodForm />
             <form onSubmit = {this.handleSubmit}>
-                <label>Image:</label>
-                <input type = "file" />
+                <label>Mineral Image:</label>
+                <input type = "file" name = "image"/>
+                <br></br>
+
+                <label>Name: </label>
+                <input type = "text" name = "name" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Description: </label>
+                <input type = "text" name = "description" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Sell Price: </label>
+                <input type = "number" name = "sellprice" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Location: </label>
+                <input type = "text" name = "location" onChange = {this.handleChange} />
+                <br></br>
+
+                <label>Used in: </label>
+                <input type = "text" name = "used_in" onChange = {this.handleChange} />
+                <br></br>
+
+
                 <input type = "submit" value = "Create" />
             </form>
             </>
@@ -28,4 +61,4 @@ class Form extends Component {
     }
 }
 
-export default connect(null, {}) (Form)
+export default connect(null, {}) (MineralForm)
