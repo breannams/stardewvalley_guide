ca55972c3672c0dd98aed4c661d72bfa14161422
breannams <breannamarie_s@hotmail.com>
2021-11-15 19:29:12 -0500

    Create state and form for shops.

--- stardew_guide_client/src/components/Forms/ShopForm.js
+++ stardew_guide_client/src/components/Forms/ShopForm.js
@@ -1,9 +1,13 @@
 import { Component } from "react";
 import {connect} from 'react-redux'
 
-class Form extends Component {
+class ShopForm extends Component {
     state = {
-
+        name : "",
+        open_hours: "",
+        days_closed: "",
+        inventory: "",
+        extra_info: ""
     }
     handleChange = (event) => {
         this.setState({
@@ -18,14 +22,37 @@ class Form extends Component {
     render(){
         return(
             <>
-            <form onSubmit = {this.handleSubmit}>
-                <label>Image:</label>
-                <input type = "file" />
-                <input type = "submit" value = "Create" />
-            </form>
+                <h1>Add a new shop: </h1>
+                <h3>Choose which Stardewvalley this shop belongs to: </h3> 
+                
+                <StardewmodForm />
+
+                <form onSubmit = {this.handleSubmit}>
+                    <label>Name:</label>
+                    <input type = "text" name = "name" onChange = {this.handleChange}/>
+                    <br></br>
+
+                    <label>Hours Open: </label>
+                    <input type = "text" name = "open_hours" onChange = {this.handleChange} />
+                    <br></br>
+
+                    <label>Days closed: </label>
+                    <input type = "text" name = "days_closed" onChange = {this.handleChange} />
+                    <br></br>
+
+                    <label>Inventory: </label>
+                    <input type = "text" name = "inventory" onChange = {this.handleChange} />
+                    <br></br>
+                    
+                    <label>Extra Info: </label>
+                    <input type = "text" name = "extra_info" placeholder = "Ex. Clint's shop allows for tool upgrades: need 5 bars of copper,iron,gold, or iridium etc." onChange = {this.handleChange} />
+                    <br></br>
+
+                    <input type = "submit" value = "Create" />
+                </form>
             </>
         )
     }
 }
 
-export default connect(null, {}) (Form)
+export default connect(null, {}) (ShopForm)
